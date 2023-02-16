e55920c8a7543c43dbad0e07acb572392b09bd57
HacKiStMwas <sammymwas116@gail.com>
2023-02-16 13:43:56 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,13 @@
-#!/bin/bash
-#include <stdio.h>
-/*
+#include	<stdio.h>
+/**
+ * main	-	Entry
  *
- * main - Entry
+ * Description:	'A program to use puts for output'
  *
- * Return: Always 0
- *
-*/
-
-int main(void)
-
+ * Return:	0
+ */
+int	main(void)
 {
-
 	puts("\"Programming is like building a multilingual puzzle");
-	
-	return (0);
+	return	(0);
 }
