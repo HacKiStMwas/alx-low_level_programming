2f3573cd6bed800a3354dcfdfa9725b78adc06a1
HacKiStMwas <sammymwas116@gail.com>
2023-02-16 13:55:21 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include	<stdio.h>
+/**
+ * main	-	Entry
+ *
+ * Description:	'A program to use printf for output'
+ *
+ * Return:	Always	0
+*/
+int	main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art\n");
+	return	(0);
+}
