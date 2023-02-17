cc4841506f1f320037a2b2eac3226688df5b76e0
HacKiStMwas <sammymwas116@gail.com>
2023-02-17 15:16:16 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Description: '7-print_tebahpla.c'
+ * Return: Always 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch <= 'a'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
