4e3a9305129725951fd137a7cfa8011b62ced359
HacKiStMwas <sammymwas116@gail.com>
2023-03-02 14:53:32 -0800

    102-magic.c

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - line of code that prints a[2] = 98
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+	int a[5];
+	int *p;
+
+	a[2] = 1024;
+	p = &i;
+	*(p = &a[2] - 1) = 98;
+	printf("a[2] = %d\n", a[2]);
+	return (0);
+}
